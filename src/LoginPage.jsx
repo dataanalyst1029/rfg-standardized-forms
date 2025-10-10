72c267930e69f56a9b75bad8df51c4edeb42bc45
dataanalyst1029 <data.analyst2@ribshackcorp.com>
2025-10-10 10:28:36 +0800

    Update LoginPage.jsx

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -13,7 +13,7 @@ function LoginPage({ onLogin }) {
     setMessage("🔄 Logging in...");
 
     try {
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch("https://rfg-standardized-forms.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
